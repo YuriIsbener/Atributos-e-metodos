56bbd7291998eafe68b547c3807e40ded704425e
Yuri Isbener <yurisuhett@gmail.com>
2020-12-01 14:35:53 -0300

    Trab feito

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Atributos_e_metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem objetoPersonagem = new Personagem();
+
+            objetoPersonagem.nome = "Tsukamoto";
+            objetoPersonagem.idade = 32;
+            objetoPersonagem.armadura = "Voadora";
+
+            Personagem objetoPersonagem2 = new Personagem();
+
+            objetoPersonagem2.nome = "Joyce";
+            objetoPersonagem2.idade = 18;
+            objetoPersonagem2.armadura = "Flash";
+
+            Console.WriteLine($"Personagem 1 = {objetoPersonagem.nome} - {objetoPersonagem.Atacar()}");
+            Console.WriteLine($"Personagem 2 = {objetoPersonagem2.nome} - {objetoPersonagem2.Defender()}");
+
+
+
+
+        }
+    }
+}
